735d82f8830fc6b117d018eedaefde8735274538
yuxxeun <itsyuxxeun@gmail.com>
2024-07-12 15:57:14 +0700

    ref: everything

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
-module.exports = {
+const mdxPlugin = require("@jamshop/eleventy-plugin-mdx");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(mdxPlugin);
+  return {
     dir: {
       input: "links",
       includes: "../_includes",
       data: "../_data"
     }
-  }
+  };
+};
